c1af993745e60a4c0bdf8ee0e3c12e2c9b0f0aa1
Askari <askarir64@gmail.com>
2025-04-19 22:33:37 +0530

    Line Charts normal and filtered

---
+++ src/Charts/LineChartComponents.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  Brush,
+} from "recharts";
+
+const data = [
+  { month: "Jan", sales: 4000, expenses: 2400 },
+  { month: "Feb", sales: 3000, expenses: 1398 },
+  { month: "Mar", sales: 2000, expenses: 9800 },
+  { month: "Apr", sales: 2780, expenses: 3908 },
+  { month: "May", sales: 1890, expenses: 4800 },
+  { month: "Jun", sales: 2390, expenses: 3800 },
+  { month: "Jul", sales: 3490, expenses: 4300 },
+];
+
+export function LineChartComponent() {
+  return (
+    <div
+      style={{
+        borderRadius: "1rem",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        padding: "1rem",
+        backgroundColor: "#fff",
+      }}
+    >
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey={"sales"}
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey={"expenses"}
+            stroke="#00ff00"
+            activeDot={{ r: 8 }}
+          />
+          <Brush />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+export function FilteredLineChart() {
+  const [filter, setFilter] = useState("sales");
+
+  return (
+    <div className=" py-2">
+      <div className="rounded-2xl shadow-md p-4 bg-white">
+        <ResponsiveContainer width="100%" height={300}>
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey={filter}
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            <Brush />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+
+      <div className="flex items-center gap-4 mt-4">
+        <label className="text-lg font-medium">Select Data:</label>
+        <select
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          className="p-2 text-base border rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+        >
+          <option value="sales">Sales</option>
+          <option value="expenses">Expenses</option>
+        </select>
+      </div>
+    </div>
+  );
+}
