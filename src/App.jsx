7e3a5b68dc8f01324f194957dd9e6feb2cb35d01
Askari <askarir64@gmail.com>
2025-04-19 22:34:07 +0530

    Line Charts Integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { LineChartComponent, FilteredLineChart } from "./Charts/LineChartComponents";
 function App() {
   return (
     <>
-      <div className="min-h-dvh w-full bg-[#003049] text-[#fdf0d5]">
+      <div className="min-h-dvh w-full bg-[#003049] text-white">
         <div className="max-w-6xl mx-auto px-4 py-12">
           <h1 className="text-center mt-12 text-xl md:text-2xl lg:text-4xl mb-12">
             Simple React Charts
           </h1>
-          <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <p className="text-center bg-[#669bbc] rounded-xl p-6">Placeholder for Chart 1</p>
-            <p className="text-center bg-[#669bbc] rounded-xl p-6">Placeholder for Chart 2</p>
-            <p className="text-center bg-[#669bbc] rounded-xl p-6">Placeholder for Chart 3</p>
+
+          <div className=" grid grid-cols-1 md:grid-cols-2 gap-4 text-gray-900">
+            <div className="text-center bg-[#669bbc] rounded-xl p-6">
+              <LineChartComponent />
+            </div>
+            <div className="text-center bg-[#669bbc] rounded-xl p-6">
+              <FilteredLineChart />
+            </div>
           </div>
         </div>
       </div>
